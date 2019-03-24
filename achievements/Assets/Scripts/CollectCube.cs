709549d21ff84ac59d75b47f57eb970eab775dba
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2019-03-24 00:38:04 -0400

    asset: Create Collection cube and script
    Create a prototype cube to collect and a script to implement the collection.

---
+++ achievements/Assets/Scripts/CollectCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCube : MonoBehaviour
+{
+
+    public AudioSource collectSound;        //used to indicate the item has been collected
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GlobalAchievements.ach01Count += 1;
+        //collectSound.Play();                  //play the sound when a source file is obtained with proper licensing
+        Destroy(gameObject);
+    }
+
+}
