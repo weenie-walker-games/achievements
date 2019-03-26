8bcb05155ebb4dcba0564aeba7f0ce2aac4c8b8b
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2019-03-25 21:21:50 -0400

    feat: Add Achievement02 - End Level

--- achievements/Assets/Scripts/GlobalAchievements.cs
+++ achievements/Assets/Scripts/GlobalAchievements.cs
@@ -23,11 +23,18 @@ public class GlobalAchievements : MonoBehaviour
     public GameObject achDesc;
 
 
-    //Achievement 01 Specific
+    //Achievement 01 Specific - Collectible
     public GameObject ach01Image;
-    public static int ach01Count;       //A static variable used to keep track of how many have already been collected.
+    public static int ach01Count;       //A static variable used to keep track of how many have already been collected; linked to CollectCube.cs
     public int ach01Trigger = 5;        //How many items need to be collected to trigger the achievement?
     public int ach01Code;               //A random integer that can be saved to PlayerPrefs and used to show if an achievement has previously been obtained
+
+
+    //Achievement 02 Specific - End Level
+    public GameObject ach02Image;
+    public static bool triggerAch02 = false;    //A static bool to trigger that this achievement has been obtained; linked to LevelFinish.cs
+    public int ach02Code;                       //A random integer that can be saved to PlayerPrefs and used to show if an achievement has previously been obtained
+
 
 
     // Start is called before the first frame update
@@ -40,11 +47,18 @@ public class GlobalAchievements : MonoBehaviour
     void Update()
     {
         ach01Code = PlayerPrefs.GetInt("Ach01");
+        ach02Code = PlayerPrefs.GetInt("Ach02");
 
-        if(ach01Count == ach01Trigger && ach01Code != 12345)            //The code 12345 is just an integer used for comparison purposes rather than using an on/off style bool.
+        if (ach01Count == ach01Trigger && ach01Code != 12345)            //The code 12345 is just an integer used for comparison purposes rather than using an on/off style bool.
         {
             StartCoroutine(Trigger01Ach());
         }
+
+        if (triggerAch02 == true && ach02Code != 12346)            //The code 12346 is just an integer used for comparison purposes rather than using an on/off style bool.
+        {
+            StartCoroutine(Trigger02Ach());
+        }
+
 
     }
 
@@ -68,6 +82,28 @@ public class GlobalAchievements : MonoBehaviour
         achDesc.GetComponent<Text>().text = "";
         achActive = false;
     }
+
+
+    IEnumerator Trigger02Ach()
+    {
+        achActive = true;
+        ach02Code = 12346;                          //Set code to show this achievement has been obtained
+        PlayerPrefs.SetInt("Ach02", ach01Code);
+        //achSound.Play();                          //Once an audio file has been obtained, uncomment out this line to use
+        ach02Image.SetActive(true);
+        achTitle.GetComponent<Text>().text = "COMPLETED!";
+        achDesc.GetComponent<Text>().text = "You made a level complete achievement!";
+        achNote.SetActive(true);
+        yield return new WaitForSeconds(7);
+
+        //Resetting UI
+        achNote.SetActive(false);
+        ach02Image.SetActive(false);
+        achTitle.GetComponent<Text>().text = "";
+        achDesc.GetComponent<Text>().text = "";
+        achActive = false;
+    }
+
 
 
     //Use this section to clear the PlayerPrefs
---
+++ achievements/Assets/Scripts/LevelFinish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFinish : MonoBehaviour
+{
+    public GameObject fadeOut;
+
+    public GameObject thePlayer;            //Adding this code to prevent the player from moving
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(EndLevel());
+    }
+
+    IEnumerator EndLevel()
+    {
+        //fadeOut.SetActive(true);                              //Once a fadeOut is implemented, un-comment out this code
+        thePlayer.GetComponent<CharControl>().enabled = false;  //This line of code will prevent the player from moving during the fadeOut
+        yield return new WaitForSeconds(3.5f);
+        GlobalAchievements.triggerAch02 = true;
+        thePlayer.GetComponent<CharControl>().enabled = true;   //Reset player movement; this code may be adjusted if we were to jump to a new scene.
+
+    }
+
+}
